96060c1fe0ebc17967554ee78f64b9eaf18e3b69
Evrard Nkono <evrardnkono870@gmail.com>
2026-06-02 11:27:26 +0200

    All

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,13 @@
-// main.jsx
+// frontend/src/main.jsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'  // ← AJOUTE CETTE LIGNE
+import { BrowserRouter } from 'react-router-dom'  // ← Doit être ICI
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>  {/* ← ENCADRE App AVEC BrowserRouter */}
+    <BrowserRouter>  {/* ← Le Router est uniquement ici */}
       <App />
     </BrowserRouter>
   </StrictMode>,
